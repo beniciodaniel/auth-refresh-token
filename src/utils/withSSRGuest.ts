d89c844c5933b70f6672df3dd5babcf07e8385d4
Benício Barreto <benicio.daniel@gmail.com>
2021-07-21 19:23:41 -0300

    created High Order Function

---
+++ src/utils/withSSRGuest.ts
@@ -0,0 +1,26 @@
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  GetServerSidePropsResult
+} from 'next';
+import { parseCookies } from 'nookies';
+
+// HOC
+export function withSSRGuest<P>(fn: GetServerSideProps<P>): GetServerSideProps {
+  return async (
+    ctx: GetServerSidePropsContext
+  ): Promise<GetServerSidePropsResult<P>> => {
+    console.log(ctx.req.cookies);
+    const cookies = parseCookies(ctx);
+    if (!cookies['nextauth.token']) {
+      return {
+        redirect: {
+          destination: '/dashboard',
+          permanent: false
+        }
+      };
+    }
+
+    return await fn(ctx);
+  };
+}
