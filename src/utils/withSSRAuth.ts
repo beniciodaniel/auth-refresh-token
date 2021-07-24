b67d2252844b5aef4265a19b0a13bf04e7ff71f0
Benício Barreto <benicio.daniel@gmail.com>
2021-07-24 11:13:41 -0300

    redirect if there is no refresh token

--- src/utils/withSSRAuth.ts
+++ src/utils/withSSRAuth.ts
@@ -3,7 +3,8 @@ import {
   GetServerSidePropsContext,
   GetServerSidePropsResult
 } from 'next';
-import { parseCookies } from 'nookies';
+import { destroyCookie, parseCookies } from 'nookies';
+import { AuthTokenError } from '../services/errors/AuthTokenError';
 
 // HOC
 export function withSSRAuth<P>(fn: GetServerSideProps<P>): GetServerSideProps {
@@ -21,6 +22,19 @@ export function withSSRAuth<P>(fn: GetServerSideProps<P>): GetServerSideProps {
       };
     }
 
-    return await fn(ctx);
+    try {
+      return await fn(ctx);
+    } catch (error) {
+      if (error instanceof AuthTokenError) {
+        destroyCookie(ctx, 'nextauth.token');
+        destroyCookie(ctx, 'nextauth.refreshToken');
+        return {
+          redirect: {
+            destination: '/',
+            permanent: false
+          }
+        };
+      }
+    }
   };
 }
