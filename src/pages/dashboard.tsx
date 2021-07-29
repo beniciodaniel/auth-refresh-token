8027e7e61fac80dd1719cfb3d0c4c84bc9d89c13
Benício Barreto <benicio.daniel@gmail.com>
2021-07-28 21:52:27 -0300

    criou hook para checar se usuario tem permissao ou roles

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,12 +1,17 @@
 import { destroyCookie } from 'nookies';
 import { useContext, useEffect } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
+import { useCan } from "../hooks/useCan";
 import { setupAPIClient } from '../services/api';
 import { api } from '../services/apiClient';
 import { withSSRAuth } from '../utils/withSSRAuth';
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
+
+  const userCanSeeMetrics = useCan({
+    roles: ['administrator', 'editor']
+  })
 
   useEffect(() => {
     api
@@ -15,7 +20,12 @@ export default function Dashboard() {
       .catch((error) => console.log(error));
   }, []);
 
-  return <h1>{user?.email}</h1>;
+  return (
+    <>
+      <h1>{user?.email}</h1>
+      {userCanSeeMetrics && <div>Métricas</div>}
+    </>
+  );
 }
 
 export const getServerSideProps = withSSRAuth(async (ctx) => {
