0bde96fa104c6149466fe3f3890f8c32501db9a9
Benício Barreto <benicio.daniel@gmail.com>
2021-08-07 17:50:11 -0300

    added validation in withSSRAuth hook

--- src/utils/withSSRAuth.ts
+++ src/utils/withSSRAuth.ts
@@ -5,21 +5,52 @@ import {
 } from 'next';
 import { destroyCookie, parseCookies } from 'nookies';
 import { AuthTokenError } from '../services/errors/AuthTokenError';
+import decode from 'jwt-decode';
+import { validateUserPermissions } from './validateUserPermission';
+
+type WithSSRAuthOptions = {
+  permissions?: string[];
+  roles?: string[];
+};
 
 // HOC
-export function withSSRAuth<P>(fn: GetServerSideProps<P>): GetServerSideProps {
+export function withSSRAuth<P>(
+  fn: GetServerSideProps<P>,
+  options?: WithSSRAuthOptions
+): GetServerSideProps {
   return async (
     ctx: GetServerSidePropsContext
   ): Promise<GetServerSidePropsResult<P>> => {
     console.log(ctx.req.cookies);
     const cookies = parseCookies(ctx);
-    if (!cookies['nextauth.token']) {
+    const token = cookies['nextauth.token'];
+    if (!token) {
       return {
         redirect: {
           destination: '/',
           permanent: false
         }
       };
+    }
+
+    if (options) {
+      const user = decode<{ permissions: string[]; roles: string[] }>(token);
+      const { permissions, roles } = options;
+
+      const userHasValidPermissions = validateUserPermissions({
+        user,
+        permissions,
+        roles
+      });
+
+      if (!userHasValidPermissions) {
+        return {
+          redirect: {
+            destination: '/dashboard',
+            permanent: false
+          }
+        };
+      }
     }
 
     try {
